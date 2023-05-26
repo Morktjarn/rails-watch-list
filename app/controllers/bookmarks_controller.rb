16e8df42e234b4c3de281aafcb6e7e61a70e8ca8
Ulysse <ulysse@ik.me>
2023-05-26 11:10:48 +0200

    watch List non fonctionnelle

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,29 @@
+class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    flash[:success] = "The bookmark was successfully destroyed."
+    redirect_to bookmarks_url
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:content, :list_id, :movie_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  get 'bookmarks/destroy'
+  get 'lists/show'
+  get 'lists/new'
+  get 'lists/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  # resources :movies
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:index, :show, :new, :create, :destroy]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
